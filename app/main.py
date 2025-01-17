9287a3a1960f3c039de52292a8c3cb02a7adeef1
microwavestine <microwavestine@gmail.com>
2025-01-17 17:08:08 +0900

    invalid command

--- app/main.py
+++ app/main.py
@@ -3,10 +3,13 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    # sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
+    print(f"{command}: command not found")
+
+
 
 
 if __name__ == "__main__":
