5ad6619e1c4b3d3aad234db492d40fc8c69d8a21
microwavestine <microwavestine@gmail.com>
2025-01-17 18:04:47 +0900

    type

--- app/main.py
+++ app/main.py
@@ -7,14 +7,14 @@ def main():
         # Wait for user input
         command = input()
         arr = command.split(" ")
-        if arr[0] == "exit" and arr[1] == "0": exit()
-        if arr[0] == "echo":
+        if arr[0] == "exit" and arr[1] == "0": break
+        elif arr[0] == "echo":
             print(" ".join(arr[1:]))
-        if arr[0] == "type":
+        elif arr[0] == "type":
             if arr[1] == "type" or arr[1] == "exit" or arr[1] == "echo":
                 print(f"{arr[1]} is a shell builtin")
             else: print(f"{arr[1]}: not found") 
-        else: print(f"{command}: command not found")
+        else: print(f"{arr[0]}: command not found")
 
 
 
