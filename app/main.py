f14fcd371b5a74f250dfebcc04cf6eee2c28efd6
microwavestine <microwavestine@gmail.com>
2025-01-17 17:26:00 +0900

    exit

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
+        if command == "exit 0": break
         print(f"{command}: command not found")
         # main()
 
