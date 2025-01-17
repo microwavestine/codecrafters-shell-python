c632f4334cb2551bf7bccb96b45333108f2eae95
microwavestine <microwavestine@gmail.com>
2025-01-18 00:32:56 +0900

    remove newline

--- app/main.py
+++ app/main.py
@@ -18,11 +18,11 @@ def main():
                 if os.path.isfile(f"{path}/{arr[1]}"):
                     current_path = f"{path}/{arr[1]}"
             if arr[1] == "type" or arr[1] == "exit" or arr[1] == "echo":
-                print(f"{arr[1]} is a shell builtin\n")
+                print(f"{arr[1]} is a shell builtin")
             elif current_path:
-                print(f"{arr[1]} is {current_path}\n")
-            else: print(f"{arr[1]}: not found\n") 
-        else: print(f"{arr[0]}: command not found\n")
+                print(f"{arr[1]} is {current_path}")
+            else: print(f"{arr[1]}: not found") 
+        else: print(f"{arr[0]}: command not found")
 
 
 
