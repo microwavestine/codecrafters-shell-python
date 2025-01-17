8c00c6ec885d003b9573ce76cab143b74b64d399
microwavestine <microwavestine@gmail.com>
2025-01-17 17:17:51 +0900

    repl

--- app/main.py
+++ app/main.py
@@ -3,12 +3,9 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    sys.stdout.flush()
     # Wait for user input
     command = input()
     print(f"{command}: command not found\n")
-    input()
-
     main()
 
 
