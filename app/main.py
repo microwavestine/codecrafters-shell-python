cfe1a63edfc360459a8b6e2d5b8ed1ab3f41ad61
microwavestine <microwavestine@gmail.com>
2025-01-19 21:09:54 +0900

    #IP1

---
+++ app/main.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import subprocess
+
+def main():
+    PATH = os.environ.get("PATH", "")
+    paths = PATH.split(":")
+    while(True):
+        sys.stdout.write("$ ")
+        # Wait for user input
+        command = input()
+        arr = command.split(" ")
+        if arr[0] == "exit" and arr[1] == "0": break
+        elif arr[0] == "echo":
+            print(" ".join(arr[1:]))
+        elif arr[0] == "type":
+            current_path = None
+            for path in paths:
+                if os.path.isfile(f"{path}/{arr[1]}"):
+                    current_path = f"{path}/{arr[1]}"
+            if arr[1] == "type" or arr[1] == "exit" or arr[1] == "echo":
+                print(f"{arr[1]} is a shell builtin")
+            elif current_path:
+                print(f"{arr[1]} is {current_path}")
+            else: print(f"{arr[1]}: not found")
+        else:
+            # Run external program
+            current_path = None
+            for path in paths:
+                # if os.path.isfile(f"{path}/{arr[1]}"):
+                #     current_path = f"{path}/{arr[1]}"
+                if os.path.isfile(command.split(" ")[0]):
+                    os.system(command)
+            else:
+                print(f"{arr[0]}: command not found")
+
+if __name__ == "__main__":
+    main()
