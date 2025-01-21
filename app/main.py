20d06d01515c89e485f29359e449b508d1cf143d
microwavestine <microwavestine@gmail.com>
2025-01-21 23:18:55 +0900

    copied

--- app/main.py
+++ app/main.py
@@ -1,38 +1,43 @@
 import sys
 import os
-import subprocess
+from pathlib import Path
+
+def find_command(command):
+    paths = os.environ.get('PATH') or ""
+    for path in map(lambda s: f"{s}/{command}", paths.split(":")):
+        if Path(path).exists():
+            return path
+
+    return None
 
 def main():
     while(True):
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        command = input()
-        arr = command.split(" ")
-        if arr[0] == "exit" and arr[1] == "0": break
-        elif arr[0] == "echo":
-            print(" ".join(arr[1:]))
-        elif arr[0] == "type":
-            current_path = None
-            PATH = os.environ.get("PATH", "")
-            paths = PATH.split(":")
-            for path in paths:
-                if os.path.isfile(f"{path}/{arr[1]}"):
-                    current_path = f"{path}/{arr[1]}"
-            if arr[1] == "type" or arr[1] == "exit" or arr[1] == "echo":
-                print(f"{arr[1]} is a shell builtin")
+        command = input().split()
+        is_program = False
+
+        if command[0] == "exit" and command[1] == "0": break
+        elif command[0] == "echo":
+            print(" ".join(command[1:]))
+        elif command[0] == "type":
+            current_path = find_command(command[1])
+            if command[1] == "type" or command[1] == "exit" or command[1] == "echo":
+                print(f"{command[1]} is a shell builtin")
             elif current_path:
-                print(f"{arr[1]} is {current_path}")
-            else: print(f"{arr[1]}: not found")
+                print(f"{command[1]} is {current_path}")
+            else: print(f"{command[1]}: not found")
         else:
             # Run external program
-            PATH = os.environ.get("PATH", "")
-            paths = PATH.split(":")
-            for path in paths:
-                if os.path.isfile(f"{path}/{arr[1]}"):
-                    os.system(command)
+            program = find_command(command[0])
+            if program:
+                isProgram = True
+                os.system(" ".join([program, *command[1:]]))
             else:
-                print(f"{arr[0]}: command not found")
-
+                print(f"{command[0]}: command not found")
+        if not is_program:
+            sys.stdout.write("\n")
+        sys.stdout.flush()
 if __name__ == "__main__":
     main()
