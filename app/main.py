412512fb2f78b0bacd0666ecf8339e9d5976cb15
microwavestine <microwavestine@gmail.com>
2025-01-21 23:10:44 +0900

    path

--- app/main.py
+++ app/main.py
@@ -5,9 +5,8 @@ import subprocess
 def main():
     while(True):
         sys.stdout.write("$ ")
+        sys.stdout.flush()
         # Wait for user input
-        PATH = os.environ.get("PATH", "")
-        paths = PATH.split(":")
         command = input()
         arr = command.split(" ")
         if arr[0] == "exit" and arr[1] == "0": break
@@ -15,6 +14,8 @@ def main():
             print(" ".join(arr[1:]))
         elif arr[0] == "type":
             current_path = None
+            PATH = os.environ.get("PATH", "")
+            paths = PATH.split(":")
             for path in paths:
                 if os.path.isfile(f"{path}/{arr[1]}"):
                     current_path = f"{path}/{arr[1]}"
@@ -25,11 +26,9 @@ def main():
             else: print(f"{arr[1]}: not found")
         else:
             # Run external program
+            PATH = os.environ.get("PATH", "")
+            paths = PATH.split(":")
             for path in paths:
-                PATH = os.environ.get("PATH", "")
-                paths = PATH.split(":")
-                command = input()
-                arr = command.split(" ")
                 if os.path.isfile(f"{path}/{arr[1]}"):
                     os.system(command)
             else:
