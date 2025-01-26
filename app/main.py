78dc512b714d9a9263ccc6565e0253223ef0ddf2
microwavestine <microwavestine@gmail.com>
2025-01-27 04:57:05 +0900

    Pass #GU3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 import sys
 import os
 from pathlib import Path
+import shlex
+import subprocess
+
+def findExecutable(command):
+    paths = os.getenv("PATH", "").split(os.pathsep)
+    for path in paths:
+        executablePath = os.path.join(path, command)
+        if os.path.isfile(executablePath):
+            return executablePath
+    return None
 
 def find_command(command):
     paths = os.environ.get('PATH') or ""
@@ -22,29 +32,44 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        command = input().split()
-        is_program = False
+
+        line = input()
+        command = line.split()
 
         if command[0] == "exit" and command[1] == "0": break
         elif command[0] == "echo":
-            print(" ".join(command[1:]))
+            print(" ".join(shlex.split(line)[1:]))
         elif command[0] == "type":
             current_path = find_path(command[1])
-            if command[1] == "type" or command[1] == "exit" or command[1] == "echo":
+            if command[1] == "type" or command[1] == "exit" or command[1] == "echo" or command[1] == "pwd":
                 print(f"{command[1]} is a shell builtin")
             elif current_path:
                 print(f"{command[1]} is {current_path}")
             else: print(f"{command[1]}: not found")
+        elif command[0] == "pwd":
+            sys.stdout.write(os.getcwd() + "\n")
+        elif command[0] == "cd":
+            path = Path(command[1])
+            if command[1].startswith("~"):
+                path = path.expanduser()
+
+            if path.exists() and path.is_dir():
+                is_program = True
+                os.chdir(path)
+            else:
+                sys.stdout.write(f"{command[0]}: {command[1]}: No such file or directory\n")
         else:
             # Run external program
             program = find_command(command[0])
             if program:
                 is_program = True
-                os.system(" ".join([program, *command[1:]]))
+                args = shlex.split(line)
+                executablePath = findExecutable(args[0])
+                if executablePath:
+                    result = subprocess.run(args, capture_output=True, text=True)
+                    print(result.stdout, end="")
             else:
                 print(f"{command[0]}: command not found")
-        # if not is_program:
-        #     sys.stdout.write("\n")
-        # sys.stdout.flush()
+        sys.stdout.flush()
 if __name__ == "__main__":
     main()
